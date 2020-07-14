af92dd541c562778d12cd5f9fa3b6d59b0a5676b
Tay CJ <cjtay@outlook.sg>
2020-07-14 18:24:05 +0800

    imac useEffect

---
+++ src/components/HookCounterOne.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function HookCounterOne() {
+    const [count, setCount] = useState(0);
+    const [value, setValue] = useState('');
+
+    const clickHandler = () => {
+        setCount((prevCount) => prevCount + 1);
+    };
+
+    const changeHandler = (e) => {
+        setValue({
+            ...value,
+            [e.target.id]: [e.target.value],
+        });
+    };
+
+    useEffect(() => {
+        document.title = `You have clicked ${count} times`;
+        console.log('component re-rendered only when state.count is changed');
+    }, [count]);
+
+    return (
+        <div>
+            <h1>
+                React Hooks Tutorial 7 - useEffect runs after every render -
+                componentDidMount and componentDidUpdate
+            </h1>
+            <button onClick={clickHandler}>Click {count} times</button>
+            <input type="text" id="value" onChange={changeHandler} />
+        </div>
+    );
+}
+
+export default HookCounterOne;
---
+++ src/components/HookTwo.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+function HookTwo() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const logMousePosition = (e) => {
+        console.log('logMousePosition function called when mouse move');
+        setX(e.clientX);
+        setY(e.clientY);
+    };
+
+    useEffect(() => {
+        console.log('useEffect called only once on Initial render');
+        window.addEventListener('mousemove', logMousePosition);
+    }, []);
+
+    return (
+        <div>
+            <h1>
+                React Hooks Tutorial 9 - useEffect only once - similar to
+                componentDidMount
+            </h1>
+            <h3>
+                X - {x} Y - {y}
+            </h3>
+        </div>
+    );
+}
+
+export default HookTwo;
